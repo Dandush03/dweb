bb391ea7f6763a5300f7aa371ea6d83d32d34e31
Daniel Laloush <d.laloush@outlook.com>
2020-07-02 12:18:11 -0400

    Add Animation and Contact Btn

--- app/javascript/components/ContactForm.jsx
+++ app/javascript/components/ContactForm.jsx
@@ -1,30 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => (
-  <div className="contact-form">
-    <div className="form-container">
-      <form action="" method="post">
-        <h1>¿Necesitas Una Pagina Web?</h1>
-        <h2>No dudes en contactarnos, solo llena el siguiente formulario</h2>
-        <div className="name">
-          <label htmlFor="firstName">
-            <input type="text" name="firstName" placeholder="Nombre" />
-          </label>
-          <label htmlFor="lastName">
-            <input type="text" name="lastName" placeholder="Apellido" />
-          </label>
+export default class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { active: false };
+    this.handlerForm = this.handlerForm.bind(this);
+  }
+
+  componentDidMount() {
+    const container = document.getElementsByName('action');
+    const form = document.getElementsByClassName('contact-form')[0];
+    form.addEventListener('click', (e) => e.stopPropagation());
+    Object.keys(container).forEach((div) => {
+      container[div].addEventListener('click', () => this.handlerForm());
+    });
+  }
+
+  handlerForm() {
+    this.setState((e) => {
+      if (e.active) {
+        window.removeEventListener('click', () => this.handlerForm());
+        return { active: false };
+      }
+      return { active: true };
+    });
+  }
+
+  render() {
+    const { state: { active } } = this;
+    if (active) {
+      window.onclick = () => this.handlerForm();
+    } else {
+      window.onclick = null;
+    }
+    return (
+      <div className={`contact-form ${active ? 'active' : 'close'}`}>
+        <div className="close-form" name="action">
+          <p>Contáctanos</p>
         </div>
-        <label htmlFor="email">
-          <input type="text" name="email" placeholder="Correo" />
-        </label>
-        <label htmlFor="phone">
-          <input type="text" name="phone" placeholder="Numero de Telf" />
-        </label>
-        <label htmlFor="motive">
-          <textarea type="text" name="motive" placeholder="Motivo" />
-        </label>
-        <button type="submit">Enviar</button>
-      </form>
-    </div>
-  </div>
-);
+        <div className="form-container">
+          <form action="" method="post">
+            <button type="button" name="action">X</button>
+            <h1>¿Necesitas una página web?</h1>
+            <h2>No dudes en contactarnos, solo llena el siguiente formulario</h2>
+            <div className="name">
+              <label htmlFor="firstName">
+                <input type="text" name="firstName" placeholder="Nombre" />
+              </label>
+              <label htmlFor="lastName">
+                <input type="text" name="lastName" placeholder="Apellido" />
+              </label>
+            </div>
+            <label htmlFor="email">
+              <input type="text" name="email" placeholder="Correo" />
+            </label>
+            <label htmlFor="phone">
+              <input type="text" name="phone" placeholder="Numero de Telf" />
+            </label>
+            <label htmlFor="motive">
+              <textarea type="text" name="motive" placeholder="Motivo" />
+            </label>
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
--- app/javascript/components/Paralax.jsx
+++ app/javascript/components/Paralax.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
 
-import ParalaxImg1 from '../../assets/images/paralax/paralax1.jpg';
-import ParalaxImg2 from '../../assets/images/paralax/paralax2.jpg';
-import ParalaxImg3 from '../../assets/images/paralax/paralax3.jpg';
-import ParalaxImg4 from '../../assets/images/paralax/paralax4.jpg';
-import ParalaxImg5 from '../../assets/images/paralax/paralax5.jpg';
-
 export default () => (
   <div className="paralax">
     <div className="cover" />
-    <div>
-      <img src={ParalaxImg1} alt="First Paralax Img" />
-    </div>
-    <div>
-      <img src={ParalaxImg2} alt="Second Paralax Img" />
-    </div>
-    <div>
-      <img src={ParalaxImg3} alt="Third Paralax Img" />
-    </div>
-    <div>
-      <img src={ParalaxImg4} alt="Fourth Paralax Img" />
-    </div>
-    <div>
-      <img src={ParalaxImg5} alt="Fifth Paralax Img" />
-    </div>
+    <div className="first" />
+    <div className="second" />
+    <div className="third" />
+    <div className="fourth" />
+    <div className="fifth" />
   </div>
 );

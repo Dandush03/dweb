b02e7a1c8c967a10d8387a330e695a3836dd11df
Daniel Laloush <d.laloush@outlook.com>
2020-07-02 21:57:45 -0400

    Fix Overflow

--- app/javascript/components/Logo.jsx
+++ app/javascript/components/Logo.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import Logo from '../../assets/images/SimpleLog.svg';
-import MobileLogo from '../../assets/images/MobileLogo.svg';
 import LogoText from '../../assets/images/LogoTexts.svg';
 
 export default () => (
   <div className="logo">
     <img src={Logo} alt="Logo Img" className="logo-img" />
     <img src={LogoText} alt="Logo Text" className="logo-text" />
-    <img src={MobileLogo} alt="Logo Mobile" className="logo-mobile" />
   </div>
 );

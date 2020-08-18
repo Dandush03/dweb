e57610d6f679c2532588a2f966724a918053b1be
Daniel Laloush <d.laloush@outlook.com>
2020-08-17 22:38:31 -0400

    Updates

---
+++ precache-manifest.beddecbb30546f99dac29eb2e6d36533.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dfd35054afb43c0ab1633e025e33404",
+    "url": "/dweb/index.html"
+  },
+  {
+    "revision": "94d9b12e7e31989b19a1",
+    "url": "/dweb/static/css/main.a623e8d1.chunk.css"
+  },
+  {
+    "revision": "f2f2467eed85e633e562",
+    "url": "/dweb/static/js/2.79ecd751.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dweb/static/js/2.79ecd751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d9b12e7e31989b19a1",
+    "url": "/dweb/static/js/main.3180e6f9.chunk.js"
+  },
+  {
+    "revision": "0d160066b838e68f59b7",
+    "url": "/dweb/static/js/runtime-main.263a457c.js"
+  },
+  {
+    "revision": "bea35746fb7cdd67b15ecf0622d8cc85",
+    "url": "/dweb/static/media/Kalam-Regular.bea35746.ttf"
+  },
+  {
+    "revision": "1dc293298184909ae35c7caa76ad9fa3",
+    "url": "/dweb/static/media/logo.1dc29329.svg"
+  },
+  {
+    "revision": "fe2bdcf567c1527a96aca3880b5c077c",
+    "url": "/dweb/static/media/person-navegating.fe2bdcf5.png"
+  }
+]);
